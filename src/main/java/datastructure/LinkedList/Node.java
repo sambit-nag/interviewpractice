e51a79a9643d53978fd6e48e28903a246822a8e9
Sambit Nag <samnag@cisco.com>
2019-10-25 07:42:45 +0530

    commiting changes to lonkedlist operations reversing a list etc

---
+++ src/main/java/datastructure/LinkedList/Node.java
@@ -0,0 +1,28 @@
+package datastructure.LinkedList;
+
+public class Node <T>{
+
+    private T data;
+    private Node next;
+
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
