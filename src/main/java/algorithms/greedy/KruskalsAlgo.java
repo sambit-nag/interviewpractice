7662c6d16ce7b3232416390c7122d85e2d34a703
Sambit Nag <samnag@cisco.com>
2019-11-19 05:35:23 +0530

    Kruskals algorithm changes

--- src/main/java/algorithms/greedy/KruskalsAlgo.java
+++ src/main/java/algorithms/greedy/KruskalsAlgo.java
@@ -5,53 +5,76 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class KruskalsAlgo {
+public class KruskalsAlgo{
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         int numOfVertices=Integer.parseInt(br.readLine());
-        int numOfedges=Integer.parseInt(br.readLine());
+        int numOfEdges=Integer.parseInt(br.readLine());
 
-        Edge edgeArr[]=new Edge[numOfedges];
+        Edge[]arrOfEdges=new Edge[numOfEdges];
 
+        for(int i=0;i<numOfEdges;i++){
 
-        for(int i=0;i<numOfedges;i++){
+            String[] edgeDetail=br.readLine().split(" ");
+            int source=Integer.parseInt(edgeDetail[0]);
+            int dest=Integer.parseInt(edgeDetail[1]);
+            int weight=Integer.parseInt(edgeDetail[2]);
 
-            String edgeInfo[]=br.readLine().split(" ");
-            int source=Integer.parseInt(edgeInfo[0]);
-            int dest=Integer.parseInt(edgeInfo[1]);
-            int weight=Integer.parseInt(edgeInfo[2]);
             Edge edge=new Edge(source,dest,weight);
-            edgeArr[i]=edge;
+            arrOfEdges[i]=edge;
 
+        }
+        List <Edge> listOfEdges=Arrays.asList(arrOfEdges);
+        Collections.sort(listOfEdges, new Comparator<Edge>() {
+            public int compare(Edge o1, Edge o2) {
+                return o1.getCost()-o2.getCost();
             }
-
-        findMinimumSpanningTree(edgeArr,numOfVertices);
+        });
+        
+        printMinimumSpanningTree(listOfEdges,numOfVertices);
 
 
     }
 
-    private static void findMinimumSpanningTree(Edge[]edgeArr,int numOfVertices) {
-        List<Integer> srcDestList=new ArrayList<Integer>();
+    private static void printMinimumSpanningTree(List<Edge> listOfEdges,int numOfVertices) {
+        int[]parent=new int[numOfVertices];
+        int mst=0;
 
-        for(int i=0;i<edgeArr.length;i++){
-            List<Edge> edgeList=Arrays.asList(edgeArr);
-            Collections.sort(edgeList, new Comparator<Edge>() {
-                public int compare(Edge o1, Edge o2) {
-                    return o1.getCost()-o2.getCost();
-                }
-            });
-        }
-        for(int i=0;i<edgeArr.length;i++){
-            if(!(srcDestList.contains(edgeArr[i].getSource())&&srcDestList.contains(edgeArr[i].getDest()))) {
-                srcDestList.add(edgeArr[i].getSource());
-                srcDestList.add(edgeArr[i].getDest());
-                System.out.println(i+" "+ edgeArr[i].getSource()+" "+edgeArr[i].getDest()+" "+edgeArr[i].getCost());
+        for(int i=0;i<numOfVertices;i++)
+            parent[i]=i;
+
+        for(int i=0;i<listOfEdges.size();i++){
+
+            int sourceVertex=listOfEdges.get(i).getSource();
+            int destVertex=listOfEdges.get(i).getDest();
+
+            int cost =listOfEdges.get(i).getCost();
+
+            if(find(sourceVertex,parent)!=find(destVertex,parent)){
+                union(sourceVertex,destVertex,parent);
+                mst+=cost;
 
             }
-
         }
 
+        System.out.println("the minimum cost of the spanning tree is "+ mst);
     }
+
+    private static int find(int vertex,int[]parent){
+        if(parent[vertex]==vertex)
+            return vertex;
+        else
+            return find(parent[vertex],parent);
+    }
+
+    private static void union(int source,int destination,int[]parent){
+
+        int x=find(source,parent);
+        int y=find(destination,parent);
+        parent[x]=y;
+
+    }
+
 
 }
